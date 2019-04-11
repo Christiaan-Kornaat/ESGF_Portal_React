7859c0fe065ff679bf0b90ac2f8aba17d9d02c52
Jan Mouwes <jan.mouwes@gmail.com>
2019-04-11 17:10:22 +0200

    Changes: - added onSelect to ESGFPropertyList - added test-functionality to XPF-wrapper

--- src/js/components/expanded-property-finder/esgf-property-list/esgf-property-list.component.jsx
+++ src/js/components/expanded-property-finder/esgf-property-list/esgf-property-list.component.jsx
@@ -3,9 +3,16 @@ import React, {Component} from "react";
 export class ESGFPropertyList extends Component {
     render() {
         let title = this.props.title;
+        let onSelect = this.props.properties.onSelect;
 
-        let items = this.props.properties.map(item =>
-            <li>{item}</li>
+        let createOnSelect = function (item) {
+            return function () {
+                onSelect(item)
+            };
+        };
+
+        let items = this.props.properties.items.map(item =>
+            <li onClick={createOnSelect(item)}>{item}</li>
         );
 
         return (

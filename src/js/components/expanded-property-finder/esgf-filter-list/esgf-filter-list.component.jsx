617fc36e152c207d29942f00dff37b93d3dd719d
chris <s1116195@student.windesheim.nl>
2019-04-12 13:08:17 +0200

    Added searchbars and more bootstrap to make the searchbars look good

--- src/js/components/expanded-property-finder/esgf-filter-list/esgf-filter-list.component.jsx
+++ src/js/components/expanded-property-finder/esgf-filter-list/esgf-filter-list.component.jsx
@@ -10,7 +10,16 @@ export class ESGFFilterList extends Component {
 
         return (
             <div className="col-sm">
-                <h3>{title}</h3>
+                <h3 className='text-center'>{title}</h3>
+
+                <div className="input-group md-form form-sm form-2 pl-0 mb-4">
+                    <input className="form-control my-0 py-1" style={{ fontSize: 12 }} type="text" placeholder="Search" aria-label="Search" />
+                    <div className="input-group-append">
+                        <span className="input-group-text cyan lighten-2" id="basic-text1"><i className="fas fa-search text-gray"
+                            aria-hidden="true"></i></span>
+                    </div>
+                </div>
+
                 <ul 
                     className="list-group list-group-flush"
                 >

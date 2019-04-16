a0541d6a7a546c250bc2971254bbb44d365bf59c
Jan Mouwes <jan.mouwes@gmail.com>
2019-04-16 17:40:45 +0200

    - Filters can be selected now - ESGFFilterServiceMock actually adds properties

--- src/js/services/esgf-filter/esgf-filter.service.mock.js
+++ src/js/services/esgf-filter/esgf-filter.service.mock.js
@@ -8,6 +8,9 @@ export class ESGFFilterServiceMock {
      * @return {ESGFFilterDTO[]}
      */
     fetch() {
-        return range(0, 5).map(number => new ESGFFilterDTO("test" + number, number ** 2));
+        let createRandomProperties = (number) => range(0, Math.ceil(Math.random() * 10)).map(item => "property" + item + " (test" + number + ")");
+        let createFilter = number => new ESGFFilterDTO("test" + number, number ** 2, createRandomProperties(number));
+
+        return range(0, Math.ceil(Math.random() * 4) + 3).map(createFilter);
     }
 }

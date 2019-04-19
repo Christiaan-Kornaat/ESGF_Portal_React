e484f11d6264dc6cbdf242ce58d6860d16d3b217
marijnkoel <marijnkoel@live.nl>
2019-04-19 11:30:56 +0200

    sidebar works

--- src/js/components/quick-filter-search/qf-sidebar/qfsidebar.component.jsx
+++ src/js/components/quick-filter-search/qf-sidebar/qfsidebar.component.jsx
@@ -3,35 +3,36 @@ import React from "react";
 export class QFSidebar extends React.Component {
     constructor(props) {
         super(props);
+
+        let { close } = props;
+
         this.state = {
             style: {
                 width: 350
-            }
+            },
         };
+        this.closeNav = close;
+        /*
         this.openNav = this.openNav.bind(this);
         this.closeNav = this.closeNav.bind(this);
+        */
     }
-
+    /*
     openNav() {
         const style = { width: 350 };
         this.setState({ style });
-        document.addEventListener("click", this.closeNav);
     }
 
     closeNav() {
-        document.removeEventListener("click", this.closeNav);
         const style = { width: 0 };
         this.setState({ style });
     }
-
+    */
     render() {
         return (
             <div className="overlay" style={this.state.style}>
                 <div className="sidenav-container">
-                    <a
-                        href="javascript:void(0)"
-                        className="closebtn"
-                        onClick={this.closeNav}>x</a>
+                    <a href="javascript:void(0)" className="closebtn" onClick={this.closeNav}>x</a>
                     <div className="text-center">
                         <h2>Presets</h2>
                         <p>Hier moeten die presets komen</p>
--- src/js/components/quick-filter-search/wrapper/qf-wrapper.component.jsx
+++ src/js/components/quick-filter-search/wrapper/qf-wrapper.component.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { QFTile } from "../esgf-qfilter-tile/qf-tile.component";
 import { QFSidebar } from "../qf-sidebar/qfsidebar.component";
+import { createWatchCompilerHost } from "typescript";
 
 export class QFWrapper extends Component {
     constructor(props) {
@@ -8,8 +9,25 @@ export class QFWrapper extends Component {
         let { filterProvider } = props;
         this._filterProvider = filterProvider;
         
+        this.state = {
+            QFSidebarShow: false,
+        }
+        
+        this.openNav = this.openNav.bind(this);
+        this.closeNav = this.closeNav.bind(this);
     }
+
+    openNav(){
+        this.setState({ QFSidebarShow: true });
+    }
+
+    closeNav(){
+        this.setState({ QFSidebarShow: false });
+    }
+
     render() {
+        let { QFSidebarShow } = this.state;
+
         let items = this._filterProvider.provide();
         let tilesInfo = [
             { title: 'Temperature', color: '#f9a718', icon: 'fas fa-thermometer-three-quarters', properties: items },
@@ -28,7 +46,9 @@ export class QFWrapper extends Component {
 
         return (
             <section className="qf-wrapper">
-                <QFSidebar/>
+                { QFSidebarShow? (
+                     <QFSidebar close={this.closeNav}/>
+                ): ""}
                 <div style={{ fontSize: 20, cursor: "pointer" }} onClick={this.openNav}>&#9776; Presets</div>
                 <div className="qf-main-container container">
                     <div className="tiles">    

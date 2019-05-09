ad83ca34b1b280a7cb1a4dc73a479f47c0298da4
marijnkoel <marijnkoel@live.nl>
2019-05-09 10:06:22 +0200

    Update xpf-column-tab-list-content.component.jsx

--- src/js/components/expanded-property-finder/column/xpf-column-tab-list-content.component.jsx
+++ src/js/components/expanded-property-finder/column/xpf-column-tab-list-content.component.jsx
@@ -53,88 +53,6 @@ class XpfColumnTabListContent extends Component {
     }
 
     /**
-     * @summary Handles the sorting buttons for the extra filter/sorting options
-     */
-    handleSortButton() {
-
-        /**
-         * @summary takes item and compares, returning true or false on Greater, less or equal which the sorting
-         *     function uses to sort filters and properties alphabetically
-         *
-         * @param {string} item
-         *
-         * @return {{isLessThan: (function(ESGFFilterDTO): boolean), isGreaterThan: (function(ESGFFilterDTO): boolean), isEqualTo: (function(ESGFFilterDTO): boolean)}}
-         *     
-         */
-        let alphabeticalComparator = (item) => {
-            let _isGreaterThan = (item2) => item.localeCompare(item2) === 1;
-            let _isLessThan = (item2) => item.localeCompare(item2) === -1;
-            let _isEqualTo = (item2) => item.localeCompare(item2) === 0;
-
-            return {
-                isGreaterThan: _isGreaterThan,
-                isLessThan: _isLessThan,
-                isEqualTo: _isEqualTo
-            };
-        };
-
-        /**
-         *
-         * @param {ESGFFilterDTO}filter
-         * @return {{isLessThan: (function(ESGFFilterDTO): boolean), isGreaterThan: (function(ESGFFilterDTO): boolean), isEqualTo: (function(ESGFFilterDTO): boolean)}}
-         *     
-         */
-        let filterComparator = filter => {
-            let base = alphabeticalComparator(filter.shortName);
-            return {
-                isGreaterThan: ({ shortName }) => base.isGreaterThan(shortName),
-                isLessThan: ({ shortName }) => base.isLessThan(shortName),
-                isEqualTo: ({ shortName }) => base.isEqualTo(shortName)
-            };
-        };
-        /**
-         *
-         * @param {ESGFFilterPropertyDTO}property
-         * @return {{isLessThan: (function(ESGFFilterPropertyDTO): boolean), isGreaterThan: (function(ESGFFilterPropertyDTO): boolean), isEqualTo: (function(ESGFFilterPropertyDTO): boolean)}}
-         *     
-         */
-        let propertyComparator = property => {
-            let base = alphabeticalComparator(property.name);
-            return {
-                isGreaterThan: ({ name }) => base.isGreaterThan(name),
-                isLessThan: ({ name }) => base.isLessThan(name),
-                isEqualTo: ({ name }) => base.isEqualTo(name)
-            };
-        };
-
-        /**
-         *
-         * @param {function(*): {isLessThan: (function(*): boolean), isGreaterThan: (function(*): boolean), isEqualTo: (function(*): boolean)}} comparator
-         *     
-         * @summary Creates a sort function using a comparator
-         * @returns {Function} function
-         */
-        let createSortFunc = (comparator) => (ascending) => {
-            let condition = (item, item2) => (ascending ?
-                (comparator(item).isGreaterThan(item2)) :
-                (comparator(item).isLessThan(item2)));
-
-            let sortMethod = (item1, item2) => !comparator(item1).isEqualTo(item2) ?
-                (condition(item1, item2) ? 1 : -1) :
-                0;
-
-            return array => array.sort(sortMethod);
-        };
-
-        let sortDirection = !this.state.sortDirection;
-        this.setState({
-            sortFunction: createSortFunc(alphabeticalComparator)(sortDirection),
-            sortDirection: sortDirection//sets a-z sort direction to the opposite
-        });
-
-    }
-
-    /**
      *
      * @param {string} newQuery
      */
@@ -181,37 +99,12 @@ class XpfColumnTabListContent extends Component {
                 </span>
             </div>);
 
-        let loadIcon = (
-            <div className="d-flex justify-content-center">
-                <div className="spinner-border" role="status">
-                    <span className="sr-only">Loading...</span>
-                </div>
-            </div>
-        );
-
         let content = (!isLoading) ?
             <UnorderedList
                 className="List"
                 items={sortFunction(renderItems)}
                 createListItem={this.createListItem} /> :
             <LoadingIcons.Spinner />;
-
-        let OptionsButton = ({ show, onClick }) => (
-            <div className="optionsButton dropdown show" role="button" id="dropdownMenuLink" aria-haspopup="true"
-                aria-expanded="false">
-                <span className="Button" onClick={onClick}>
-                    <i className="fas fa-ellipsis-h" />
-                </span>
-                <div className={"dropdown-menu dropdown-menu-right " + (show ? "show" : "")}
-                    aria-labelledby="dropdownMenuLink">
-                    <a className="dropdown-item" onClick={() => this.handleSortButton()}>Sort a-z <i
-                        className={this.state.sortDirection ?
-                            "fas fa-angle-up sortingArrow" :
-                            "fas fa-angle-down sortingArrow"}></i> </a>
-                    {getOptionButtons}
-                </div>
-            </div>
-        );
 
         return (
             <div>

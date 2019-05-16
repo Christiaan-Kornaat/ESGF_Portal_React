a3aba64ed2a06d3075b0b856fd04287f154de65d
Jan Mouwes <jan.mouwes@gmail.com>
2019-05-16 15:51:19 +0200

    Fixed some UnorderedList rerenders

--- src/js/components/shared/list-html/list-html.component.tsx
+++ src/js/components/shared/list-html/list-html.component.tsx
@@ -1,6 +1,11 @@
-import React, {PureComponent} from "react";
+import * as React from "react";
+import {Component} from "react";
 
-class HtmlList extends PureComponent {
+class HtmlList<T = any> extends Component<{ items: T[], createListItem: (T) => any, className: string }> {
+
+    state: { items: T[] };
+    private readonly _tagName: any;
+    private readonly _createListItem: (T) => Element;
 
     constructor(props, tagName) {
         super(props);
@@ -13,12 +18,16 @@ class HtmlList extends PureComponent {
             throw new Error("Invalid argument props.tagName. Must be one of " + TAG_WHITELIST.join(", "));
         }
 
-        this.tagName = tagName;
-        this.createListItem = createListItem;
+        this._tagName = tagName;
+        this._createListItem = createListItem;
 
         this.state = {
             items: items
         };
+    }
+
+    shouldComponentUpdate({items}: Readonly<{ items: T[]; createListItem: (T) => any; className: string }>, nextState: Readonly<{}>, nextContext: any): boolean {
+        return !(items.length === 0 && items.length === this.state.items.length);
     }
 
     /**
@@ -34,9 +43,9 @@ class HtmlList extends PureComponent {
     render() {
         let {items} = this.state;
 
-        let Tag = this.tagName;
+        let Tag = this._tagName;
 
-        let listItems = items.map(this.createListItem);
+        let listItems = items.map(this._createListItem);
 
         return (
             <Tag className={this.props.className}>

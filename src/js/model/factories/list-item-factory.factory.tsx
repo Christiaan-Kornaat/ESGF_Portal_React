f856f1df24971f774af788cc7698610c4d61e6b6
chris <s1116195@student.windesheim.nl>
2019-05-28 16:37:28 +0200

    added a list item factory

--- src/js/model/factories/list-item-factory.factory.tsx
+++ src/js/model/factories/list-item-factory.factory.tsx
@@ -4,6 +4,7 @@ import Buttons from "../../components/shared/buttons/buttons.component";
 import ESGFFilterPropertyDTO from "../dto/esgf-filter-property.dto";
 import EsgfFilterPropertyVM from "../view-model/esgf-filter-property.viewmodel";
 import ArrowIcons from "../../components/shared/icons/arrow-icons.component";
+import { PresetDTO } from "../dto/esgf-preset.dto";
 
 export default class ListItemFactoryFactory {
     /**
@@ -24,13 +25,36 @@ export default class ListItemFactoryFactory {
                     className={checked ? "selected property" : "property"}
                     onClick={() => onClick(property)}>
                     <input className={"checkbox"}
-                           type={"checkbox"}
-                           onChange={() => null} //prevents error message
-                           checked={checked}/>
-                    <Buttons.Info onClick={onInfoClick}/>
+                        type={"checkbox"}
+                        onChange={() => null} //prevents error message
+                        checked={checked} />
+                    <Buttons.Info onClick={onInfoClick} />
                     {viewModel.name}
                 </li>
             );
+        };
+    }
+
+    /**
+     *
+     * @param {function(PresetDTO): void} onClick
+     * @param {function(PresetDTO): function(): void} createOnInfoClick
+     * @param {function(PresetDTO): function(): void} createOnEditClick
+     *
+     * @return {function(PresetDTO): Element}
+     */
+    createPresetListItemFactory(onClick: (preset: PresetDTO) => void, createOnInfoClick, createOnEditClick) {
+        return (preset: PresetDTO) => {
+            let onInfoClick = createOnInfoClick(preset);
+            let onEditClick = createOnEditClick(preset);
+
+            return <li key={preset.title}
+                className="filter"
+                onClick={() => onClick(preset)}>
+                {preset.title}
+                <Buttons.Primary onClick={onEditClick} title="Edit" />
+                <Buttons.Info onClick={onInfoClick} />
+            </li>;
         };
     }
 
@@ -42,8 +66,8 @@ export default class ListItemFactoryFactory {
      */
     createFilterListItemFactory(onClick: (ESGFFilterDTO) => void) {
         return filter => <li key={filter.shortName}
-                             className="filter"
-                             onClick={() => onClick(filter)}>
+            className="filter"
+            onClick={() => onClick(filter)}>
             {StringFormatter.toHumanText(filter.shortName)}
         </li>;
     }
@@ -57,8 +81,8 @@ export default class ListItemFactoryFactory {
     createQuickFilterListItem(item: ESGFFilterPropertyDTO) {
 
         let createSliceWord = (nLetters: number) => (word: string) => word.split("")
-                                                                          .slice(0, nLetters)
-                                                                          .join("");
+            .slice(0, nLetters)
+            .join("");
 
         let smallWord = createSliceWord(3)(item.filter.shortName);
 
@@ -91,12 +115,12 @@ export default class ListItemFactoryFactory {
             <li key={`${item.filter}-${item.name}`}
                 className="qf-property">
                 <span className="name">
-                    <ArrowIcons.Trash onClick={()=>onClick(item)}/>
+                    <ArrowIcons.Trash onClick={() => onClick(item)} />
                     {StringFormatter.toHumanText(item.name)} <span
                         className={"float-right text-right mr-1"}>({smallWord})</span>
                 </span>
             </li>
         );
-    }; 
+    };
 
 }
